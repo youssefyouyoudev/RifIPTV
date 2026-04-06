addddb4eb8df63747745ee3359d56c9b8ccce77b
youssefyouyoudev <contact@youssefyouyou.com>
2026-04-06 10:52:01 +0100

    ddd

--- index.php
+++ index.php
@@ -5,16 +5,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Maintenance mode
+// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Autoload
-require __DIR__.'/../vendor/autoload.php';
+// Register the Composer autoloader...
+require __DIR__.'/vendor/autoload.php';
 
-// Bootstrap app
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/bootstrap/app.php';
 
-// Run app
 $app->handleRequest(Request::capture());
